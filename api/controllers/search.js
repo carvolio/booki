58b9e395299cef0943de348ed67d070c83846106
carvolio <gui.car.dev@gmail.com>
2024-09-16 15:44:28 -0300

    create bookDetails

--- api/controllers/search.js
+++ api/controllers/search.js
@@ -6,20 +6,55 @@ const options = {
     headers: headers
 };
 
+const bookDetails = [];
+
+const getDescriptionGenetetor = () => {
+    return "blá blá blá"
+};
+
+const getDescriptionWiki = async (title) => {
+    const urlWiki = `https://en.wikipedia.org/api/rest_v1/page/summary/${title}`;
+    const response = await fetch(urlWiki, options);
+    const data = await response.json();
+    
+    if (!data.description) { 
+        return getDescriptionGenetetor();
+    } else if (data.description.includes("novel" || "novella" || "book" || "romance")) {
+        return data.extract;
+    } else {
+        return getDescriptionGenetetor();
+    };
+};
+
 const getBookOpen = async (urlOpen) => {
     const response = await fetch(urlOpen, options);
     const data = await response.json();
     const info = data.docs;
 
     for (let i of info) {
-        console.log(i.author_name[0]);
+        const bookDetail = {
+            author: i.author_name[0],
+            cover: i.cover_i,
+            publishYear: i.first_publish_year,
+            isbn: i.isbn[0],
+            language: i.language,
+            title: i.title,
+            pages: i.number_of_pages_median,
+            publisher: i.publisher[0],
+            subject: i.subject,
+            ratingCount: i.ratings_count,
+            ratingAverage: i.ratings_average,
+            description: await getDescriptionWiki(i.title)
+          };
+          bookDetails.push(bookDetail);
     };
-
-    return info;
-}
+    
+    console.log(bookDetails[0]);    
+    return bookDetails;
+};
 
 const search = async (book) => {
-    let urlOpen = `https://openlibrary.org/search.json?q=${book}&limit=1`;
+    let urlOpen = `https://openlibrary.org/search.json?q=${book}&limit=3`;
     
     return await getBookOpen(urlOpen);
 };
